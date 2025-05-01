632bbdc7f82cea66da0d4a9ee26876cd58274599
VTrung19 <trung19092003@gmail.com>
2025-05-01 14:23:41 +0700

    initial commit

---
+++ KafkaProducer/ProducerHN.py
@@ -0,0 +1,47 @@
+import json
+import time
+import ProducerConnection as connnection
+
+# Create Producer Kafka
+producer = connnection.getConnection()
+topicName = "InvoiceTopic"
+count = 0
+
+# Load data from JSON file
+with open("data/transactions_data_HN.json", encoding="utf-8") as jsonfile:
+    transactions = json.load(jsonfile)
+    startTime = time.time()
+
+    for transaction in transactions:
+        transaction_id = transaction["TransactionID"]
+        customer_id = transaction["CustomerID"]
+        timestamp = transaction["Timestamp"]
+        items = transaction["Items"]
+
+        for item in items:
+            data = {
+                "TransactionID": transaction_id,
+                "CustomerID": customer_id,
+                "Timestamp": timestamp,
+                "ProductID": item["ProductID"],
+                "Price": item["Price"],
+                "Quantity": item["Quantity"],
+                "TotalPrice": item["Price"] * item["Quantity"],
+                "Store": "HN"
+            }
+
+            # Send message to kafka
+            producer.send(topicName, data)
+            producer.flush()
+            count += 1
+            print(f"Sent item {item['ProductID']} of transaction \"{transaction_id}\" to Kafka Topic \"{topicName}\"")
+
+    # Send done message
+    producer.send(topicName, {"status": "Done"})
+    producer.flush()
+    print(f"HN Store: {count} items had sent successfully!")
+
+    executionTime = time.time() - startTime
+    print(f"Total time: {executionTime:.2f} seconds")
+
+connnection.closeConnection(producer)
